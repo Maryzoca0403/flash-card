2e1fddbfd0d95f18c3a15b3184329122ab3d70ad
Maryzoca0403 <marianykowalski@gmail.com>
2025-06-24 09:43:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação' ,
+    'o que é Javascript?' ,
+    'o javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia' ,
+    'qual é a capital do Paraná' ,
+    'a capital do Paraná é curitiba'
+)
+
+criaCartao(
+    'progamação' ,
+    'o que é python?' ,
+    'o python é uma linguagem de programação' ,
+)
